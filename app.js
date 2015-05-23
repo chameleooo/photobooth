1bde3d61c85f6e607c4d8499dac367fe9b3b91ef
Victor LEROY <vicleroy@gmail.com>
2015-05-23 19:45:06 +0200

    Animation

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var fourPictures = require('./routes/four-pictures');
 
 var app = express();
 
@@ -26,7 +26,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/four-pictures', fourPictures);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
