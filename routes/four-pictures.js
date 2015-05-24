061515ab15326bd6ec398b5d8f9d03d62d8ad49d
Victor LEROY <vicleroy@gmail.com>
2015-05-24 16:35:35 +0200

    Add raspbian-mjpeg

--- routes/four-pictures.js
+++ routes/four-pictures.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+var raspbianMjpeg = require('raspbian-mjpeg');
+
+var mJpeg = new raspbianMjpeg({
+    statusFilePath: 'status_mjpeg.txt',
+    fifoFilePath: 'FIFO',
+    mJpegFilePath: '/run/shm/mjpeg/cam.jpg',
+    mediaFolder: 'media/',
+    fps: 25
+});
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('four-pictures', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    mJpeg.startCamera(function () {
+        res.render('four-pictures', {title: 'Express'});
+    });
 });
 
 module.exports = router;
