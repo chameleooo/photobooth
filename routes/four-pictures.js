68c49bb01159cb1c319adf038ad0cc04cdf2a90c
Victor LEROY <vicleroy@gmail.com>
2015-07-01 23:38:44 +0200

    It works !

--- routes/four-pictures.js
+++ routes/four-pictures.js
@@ -17,6 +17,28 @@ GPhoto.list(function (list) {
     console.log('Found', camera.model);
 });
 
+var effects = [
+    {effect: "none"},
+    {effect: "vintage"},
+    {effect: "lomo"},
+    {effect: "clarity"},
+    {effect: "sinCity"},
+    {effect: "sunrise"},
+    {effect: "crossProcess"},
+    {effect: "orangePeel"},
+    {effect: "love"},
+    {effect: "grungy"},
+    {effect: "jarques"},
+    {effect: "pinhole"},
+    {effect: "oldBoot"},
+    {effect: "glowingSun"},
+    {effect: "hazyDays"},
+    {effect: "herMajesty"},
+    {effect: "nostalgia"},
+    {effect: "hemingway"},
+    {effect: "concentrate"}
+];
+
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -36,7 +58,20 @@ router.get('/', function (req, res, next) {
         }, function (err, stdout, stderr) {
             if (err) throw err;
             fs.writeFileSync(newDir + "/preview.jpg", stdout, 'binary');
-            res.render('four-pictures', {picture: "/media/" + time + "/preview.jpg"});
+            im.resize({
+                srcData: fs.readFileSync(newDir + "/preview.jpg", 'binary'),
+                height: 120,
+                width: 180
+            }, function (err, stdout, stderr) {
+                if (err) throw err;
+                fs.writeFileSync(newDir + "/thumbnail.jpg", stdout, 'binary');
+                res.render('four-pictures',
+                    {
+                        preview: "/media/" + time + "/preview.jpg",
+                        thumbnail: "/media/" + time + "/thumbnail.jpg",
+                        effects: effects
+                    });
+            });
         });
     });
 });
